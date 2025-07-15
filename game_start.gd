0b191c300b650d81a67e7a68c86b3f75ddca58bd
GianlucaBenesso <gianluca.benesso@gmail.com>
2025-07-15 07:16:33 +0200

    Auggiunto audio intro

--- game_start.gd
+++ game_start.gd
@@ -1,4 +1,5 @@
 extends Control
+@onready var audio_intro: AudioStreamPlayer2D = $Audio_intro
 
 
 func _on_button_pressed() -> void:
@@ -7,3 +8,9 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _ready() -> void:
+	audio_intro.finished.connect(func():
+		
+		audio_intro.play())
